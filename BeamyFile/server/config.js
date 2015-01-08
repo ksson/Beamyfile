176865207bd1e358a8541fffa393aed6bbf12183
ksson <micke_ksson@hotmail.com>
2015-01-08 13:32:16 +0100

    Update config.js

--- BeamyFile/server/config.js
+++ BeamyFile/server/config.js
@@ -5,6 +5,7 @@ ServiceConfiguration.configurations.remove({
 });
 ServiceConfiguration.configurations.insert({
   service: "google",
-  clientId: "1003739263681-f2at289i2ksb8i5e166bbgsqegn30070.apps.googleusercontent.com",
-  secret: "b2D1AFw7rmopAnKp-sHM0NCl"
+  clientId: "",
+  secret: ""
 });
+
